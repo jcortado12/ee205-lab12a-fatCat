5a90a3696c10787cb201b01d059a75c1f84626cd
jcortado <jcortado@hawaii.edu>
2022-04-16 21:05:52 -1000

    Basic working program

---
+++ Weight.cpp
@@ -0,0 +1,12 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  ee205-lab12a-fatCat - EE 205 - Spr 2022
+///
+/// @file Weight.cpp
+/// @version 1.0
+///
+/// @author Jordan Cortado <jcortado@hawaii.edu>
+/// @date   16_Apr_2022
+///////////////////////////////////////////////////////////////////////////////
+
+#include "Weight.h"
